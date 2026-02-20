85d5b8b160faa376559b2d1a4d4264b0bad131b2
Hussen Aldhb <hussenaldhb1@gmail.com>
2026-02-20 23:16:57 +0300

    Update subjects-data.js

--- js/subjects-data.js
+++ js/subjects-data.js
@@ -5,76 +5,86 @@ const SUBJECTS = [
         icon: 'fas fa-flask', 
         file: 'pages/che.html', 
         totalVideos: 57, 
-        color: '#6b21a5',
-        storageKey: 'chem_watched'
+        avgDuration: 36,
+        color: '#fa709a',
+        storageKey: 'chem_watched',
+        examDate: '2026-06-28'
     },
     { 
         id: 'phy', 
         name: 'الفيزياء', 
         icon: 'fas fa-bolt', 
         file: 'pages/phy.html', 
-        totalVideos: 129, 
-        color: '#0b4f6c',
-        storageKey: 'phys_watched'
+        totalVideos: 122, 
+        avgDuration: 36,
+        color: '#4facfe',
+        storageKey: 'phys_watched',
+        examDate: '2026-06-06'
     },
     { 
         id: 'ar', 
         name: 'العربية', 
         icon: 'fas fa-pen-fancy', 
         file: 'pages/ar.html', 
-        totalVideos: 75, 
-        color: '#2e7d32',
-        storageKey: 'arabic_watched'
+        totalVideos: 74, 
+        avgDuration: 25,
+        color: '#f59e0b',
+        storageKey: 'arabic_watched',
+        examDate: '2026-06-25'
     },
     { 
         id: 'sci', 
         name: 'العلوم', 
         icon: 'fas fa-dna', 
         file: 'pages/sci.html', 
-        totalVideos: 133, 
-        color: '#b45309',
-        storageKey: 'sci_watched'
+        totalVideos: 144, 
+        avgDuration: 53,
+        color: '#10b981',
+        storageKey: 'sci_watched',
+        examDate: '2026-06-16'
     },
     { 
         id: 'isl', 
-        name: 'الديانة', 
+        name: 'التربية الإسلامية', 
         icon: 'fas fa-mosque', 
         file: 'pages/isl.html', 
         totalVideos: 100, 
-        color: '#7b2c2c',
-        storageKey: 'deen_watched'
+        avgDuration: 22,
+        color: '#30cfd0',
+        storageKey: 'deen_watched',
+        examDate: '2026-06-20'
     },
     { 
         id: 'en', 
         name: 'الإنجليزية', 
         icon: 'fas fa-globe', 
         file: 'pages/en.html', 
-        totalVideos: 62, 
-        color: '#7e22ce',
-        storageKey: 'engWatched'
+        totalVideos: 120, 
+        avgDuration: 17,
+        color: '#ec4899',
+        storageKey: 'engWatched',
+        examDate: '2026-06-09'
     },
     { 
         id: 'fr', 
         name: 'الفرنسية', 
         icon: 'fas fa-language', 
         file: 'pages/fr.html', 
-        totalVideos: 150, 
-        color: '#1e3a8a',
-        storageKey: 'french_ultime_progress'
+        totalVideos: 134, 
+        avgDuration: 12,
+        color: '#667eea',
+        storageKey: 'french_ultime_progress',
+        examDate: '2026-06-11'
     },
     { 
         id: 'math', 
         name: 'الرياضيات', 
         icon: 'fas fa-square-root-alt', 
         file: 'pages/ma.html', 
-        totalVideos: 339, 
-        color: '#854d0e',
-        storageKey: 'mathWatched'
+        totalVideos: 591, 
+        avgDuration: 15,
+        color: '#43e97b',
+        storageKey: 'mathWatched',
+        examDate: '2026-06-14'
     }
 ];
-
-// في حال احتجت دالة تحميل إضافية
-async function loadSubjectsData() {
-    // يمكن تركها فارغة إذا لم تستخدم fetch
-    return SUBJECTS;
-}
